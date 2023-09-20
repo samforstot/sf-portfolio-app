60cc35c506790ea51837615415d4538efc435d7c
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-20 14:44:38 -0400

    About me component has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
