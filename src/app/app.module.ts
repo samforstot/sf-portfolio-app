51e75fc96c2184dbd5bfaead1eddf4719dbedae6
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-21 16:18:11 -0400

    made each a proper section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
