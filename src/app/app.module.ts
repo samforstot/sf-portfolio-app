eee28ac8575a3c8f86f97a1b916eab1dc83d086b
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-22 00:40:20 -0400

    created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AboutMeComponent,
-    IntroComponent
+    IntroComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
