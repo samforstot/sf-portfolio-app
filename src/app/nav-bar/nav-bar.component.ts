2656d67e35fb5f4c3c89a2c13c82e4efb91b4ace
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-23 01:05:35 -0400

    nav bar now adjusts when scrolling

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, HostListener } from '@angular/core';
 import { SharedService } from '../shared.service';
 
 @Component({
@@ -10,6 +10,15 @@ export class NavBarComponent {
   constructor(private sharedService: SharedService){}
 
   isShown = false;
+  isScrolled = false;
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+    if (window.scrollY > 100){
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 
   scrollToDiv(div: string){
     console.log('In Nav Bar');
