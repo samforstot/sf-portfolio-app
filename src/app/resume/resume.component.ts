834637ec96cdc2992a589a90bbf739b05007de56
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-25 00:43:05 -0400

    can successfully toggle resume

---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
+
+@Component({
+  selector: 'app-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent {
+  isShown = false; 
+  constructor(private sharedService: SharedService){}
+
+  ngOnInit(){
+    this.sharedService.getScrollSection().subscribe((value: string) => {
+      console.log('In Resume')
+      this.toggleResume(value);
+    })
+  }
+
+  toggleResume(value: string){
+    if(value == 'resume'){
+      this.isShown = !this.isShown;
+    }
+  }
+}
