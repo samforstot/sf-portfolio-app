8525fb1e783592660910535f0b10ddc4662a50b8
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-26 12:25:27 -0400

    calculator component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperimentsPageComponent } from './experiments-page/experiments-page.component';
 import { HomeComponent } from './home/home.component';
+import { CalculatorComponent } from './experiments-page/calculator/calculator.component';
+
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path:'Experiments', component: ExperimentsPageComponent}
+  {path:'Experiments', component: ExperimentsPageComponent},
+  {path:'Experiments/Calculator', component: CalculatorComponent}
 ];
 
 @NgModule({
