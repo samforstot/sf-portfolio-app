2ec7765262b4d33f9511ccac015f4b8c249368f1
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-26 23:49:54 -0400

    careted pretty calculator sorta

--- src/app/experiments-page/calculator/calculator.component.ts
+++ src/app/experiments-page/calculator/calculator.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent {
+  buttons = [
+    {name: 'AC', value: ''},
+    {name: 'negate', value: ''},
+    {name: 'percent', value: ''},
+    {name: 'divide', value: ''},
+    {name: 'seven', value: '7'},
+    {name: 'eight', value: '8'},
+    {name: 'nine', value: '9'},
+    {name: 'multiply', value: ''},
+    {name: 'four', value: '4'},
+    {name: 'five', value: '5'},
+    {name: 'six', value: '6'},
+    {name: 'minus', value: ''},
+    {name: 'one', value: '1'},
+    {name: 'two', value: '2'},
+    {name: 'three', value: '3'},
+    {name: 'plus', value: ''},
+    {name: 'zero', value: '0'},
+    {name: 'decimal', value: ''},
+    {name: 'equal', value: ''},
+  ]
+
+  clickButton(value: string){
+    console.log(value)
+  }
 
 }
