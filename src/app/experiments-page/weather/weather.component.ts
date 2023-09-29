aeb1ed4f0671776c3340d6d191015982e79b0e9c
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-09-29 00:31:08 -0400

    working weather widget

--- src/app/experiments-page/weather/weather.component.ts
+++ src/app/experiments-page/weather/weather.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent {
-
+  searchedCity = '';
+  CityData: any; 
   WeatherData: any;
   constructor(){};
 
@@ -15,13 +16,29 @@ export class WeatherComponent {
       main : {},
       isDay: true
     };
-    
-    this.getWeather();
+
+    this.getWeather("29.65140", "-82.32520");
     console.log(this.WeatherData)
   }
+
+  getCity(input: string){
+    this.searchedCity = input; 
+    //console.log(this.searchedCity)
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q='+this.searchedCity+'&limit=5&appid=20daefffe6b92667a681bcef3be17cc0').then(response=>response.json()).then(data=>{this.setGeo(data);});
+  }
   
-  getWeather(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=29.65140&lon=-82.32520&appid=20daefffe6b92667a681bcef3be17cc0').then(response=>response.json()).then(data=>{this.setWeather(data);})
+  
+  getWeather(lat: string, lon: string){
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=20daefffe6b92667a681bcef3be17cc0').then(response=>response.json()).then(data=>{this.setWeather(data);})
+  }
+
+  setGeo(data:any){
+    this.CityData = data;
+    console.log(this.CityData[0].name)
+    console.log(this.CityData[0].lon)
+    console.log(this.CityData[0].lat)
+    this.getWeather(this.CityData[0].lat, this.CityData[0].lon)
+    //this.getWeather(this.CityData.lat, this.CityData.long)
   }
 
   setWeather(data: any){
@@ -30,10 +47,10 @@ export class WeatherComponent {
     this.WeatherData.sunset_time = timeSunset.toLocaleTimeString();
     let currentDate = new Date(); 
     this.WeatherData.isDay = (currentDate.getTime() < timeSunset.getTime());
-    this.WeatherData.temp_celsius = (this.WeatherData.main.temp - 273.15).toFixed(0);
-    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    this.WeatherData.temp_max = (this.WeatherData.main.temp_max- 273.15).toFixed(0);
-    this.WeatherData.temp_feels_like = (this.WeatherData.main.temp_feels_like - 273.15).toFixed(0);
+    this.WeatherData.temp = ((this.WeatherData.main.temp - 273.15) * 9/5 + 32).toFixed(0);
+    this.WeatherData.temp_min = ((this.WeatherData.main.temp_min - 273.15) * 9/5 + 32).toFixed(0);
+    this.WeatherData.temp_max = ((this.WeatherData.main.temp_max - 273.15) * 9/5 + 32).toFixed(0);
+    this.WeatherData.temp_feels_like = ((this.WeatherData.main.feels_like - 273.15) * 9/5 + 32).toFixed(0);
   }
 
 }
